80991e87865971c912f7fbc456eb12af7828b42f
Vincent Dizon <vince@kampo.ph>
2021-06-08 10:48:23 +0800

    added projects

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { ThemeProvider, CssBaseline } from '@material-ui/core';
+import { ThemeProvider, CssBaseline, Box } from '@material-ui/core';
 import Navbar from 'layouts/Navbar';
 import theme from 'theme/theme';
 
@@ -8,6 +8,16 @@ const App = () => {
       <CssBaseline />
       <Navbar />
       <h1>Hello my new portfolio is under development.</h1>
+      <Box display="flex" flexDirection="column" justifyContent="center" alignItems="center" >
+        <h1>Recent Projects</h1>
+        <ul>
+          <li>E-comm: https://e-commerce-vncntdzn.vercel.app/</li>
+          <li>HMI website: https://www.hmi.com.ph/</li>
+          <li>Traderly website: https://traderly.vercel.app/</li>
+          <li>Anime: https://anime-vpdizon.vercel.app/</li>
+          <li>RTL: https://rtl-react.vercel.app/</li>
+        </ul>
+      </Box>
     </ThemeProvider>
   );
 }
